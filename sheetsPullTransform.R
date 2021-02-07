519d274514358c77ee7ce106c00d0be63a5ab8ce
Guerra <paul.guerra@AGR.GC.CA>
2021-02-07 08:07:21 -0500

    added budget sheet

--- sheetsPullTransform.R
+++ sheetsPullTransform.R
@@ -3,9 +3,28 @@
 
 library(googlesheets4)
 
-transactions <- read_sheet(ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", sheet = "transactionLanding", col_types = "_cD_c__ddcc__")
+# read transactions data and basic manipulation
+
+transactions <- 
+  read_sheet(ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
+             sheet = "transactionLanding", 
+             col_types = "_cD_c__ddcc__")
 
 transactions %>% 
   replace_na( list("transactionDebit" = 0, "transactionCredit" = 0)) %>% 
   mutate("transactionBalance" = transactionCredit + transactionDebit) 
   
+# read budget sheet
+
+budget <- 
+  read_sheet(ss = "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
+             sheet = "budgetAnnual", 
+             col_types = "cdc")
+
+budget %>%
+  mutate("budgetMonthly" = annualValueEst / 12)
+
+# create monthly summary table and compare to budget
+
+monthlyTracking %>% group_by(transactions,)
+
