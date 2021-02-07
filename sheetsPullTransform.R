81b833491a530c379d44be75588f6241b0155a66
Guerra <paul.guerra@AGR.GC.CA>
2021-02-07 09:44:40 -0500

    summary table created

--- sheetsPullTransform.R
+++ sheetsPullTransform.R
@@ -2,17 +2,20 @@
 # access sheets, transform and create or update sheets
 
 library(googlesheets4)
+library(lubridate)
+library(dplyr)
+library(tidyr)
 
 # read transactions data and basic manipulation
 
 transactions <- 
   read_sheet(ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
              sheet = "transactionLanding", 
-             col_types = "_cD_c__ddcc__")
-
-transactions %>% 
+             col_types = "_cD____ddcc__") %>% 
   replace_na( list("transactionDebit" = 0, "transactionCredit" = 0)) %>% 
-  mutate("transactionBalance" = transactionCredit + transactionDebit) 
+  mutate("transactionBalance" = transactionCredit + transactionDebit) %>%
+  mutate("trDate" = format(as.Date(transactionDate), "%Y-%m")) %>%
+  within(rm(transactionCredit, transactionDebit, transactionDate))
   
 # read budget sheet
 
@@ -26,5 +29,8 @@ budget %>%
 
 # create monthly summary table and compare to budget
 
-monthlyTracking %>% group_by(transactions,)
+monthlyTracking <- transactions %>%
+  group_by(trDate, transactionType, transactionSubType) %>%
+  summarise(monthlyAmt = sum(transactionBalance)) %>%
+  pivot_wider(names_from = trDate,values_from = monthlyAmt, values_fill= 0)
 
