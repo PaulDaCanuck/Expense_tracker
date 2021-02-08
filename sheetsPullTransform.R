1f9b31201ebe6f94d8807bf1adea227d541fcae1
Guerra <paul.guerra@AGR.GC.CA>
2021-02-08 13:52:25 -0500

    added comparison to monthly budget

--- sheetsPullTransform.R
+++ sheetsPullTransform.R
@@ -2,9 +2,11 @@
 # access sheets, transform and create or update sheets
 
 library(googlesheets4)
+library(googledrive)
 library(lubridate)
 library(dplyr)
 library(tidyr)
+library(scal)
 
 # read transactions data and basic manipulation
 
@@ -22,15 +24,33 @@ transactions <-
 budget <- 
   read_sheet(ss = "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
              sheet = "budgetAnnual", 
-             col_types = "cdc")
-
-budget %>%
+             col_types = "cdc")  %>%
   mutate("budgetMonthly" = annualValueEst / 12)
 
-# create monthly summary table and compare to budget
+# create monthly summary table grouped by transaction categories and month  Detail df includes subType
 
 monthlyTracking <- transactions %>%
+  group_by(trDate, transactionType) %>%
+  summarise(monthlyAmt = sum(transactionBalance)) 
+
+# %>% pivot_wider(names_from = trDate, values_from = monthlyAmt, values_fill= 0)  
+# %>% write_sheet(ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", sheet = "monthlyTracking")
+
+monthlyTrackingDetail <- transactions %>%
   group_by(trDate, transactionType, transactionSubType) %>%
-  summarise(monthlyAmt = sum(transactionBalance)) %>%
-  pivot_wider(names_from = trDate,values_from = monthlyAmt, values_fill= 0)
+  summarise(monthlyAmt = sum(transactionBalance))  
+
+
+
+# compare monthly ammounts to budget expectations
+
+monthlyCompare <- merge(x = monthlyTracking, y = budget, by = "transactionType" ) %>%
+  within(rm(annualValueEst)) %>%
+  mutate("absDiff" = monthlyAmt + budgetMonthly) %>%
+  mutate("pctDiff" = 100 * (1 - ((monthlyAmt + budgetMonthly) / budgetMonthly)))
+
+
+
+
+
 
