9c5613731657db91dd1da2b9da0ff94d3e640902
Guerra <paul.guerra@AGR.GC.CA>
2021-03-02 13:26:09 -0500

    updated ytdvis

--- sheetsPullTransform.R
+++ sheetsPullTransform.R
@@ -13,18 +13,16 @@ library(forcats)
 theme_set(theme_minimal())
 
 # read transactions data and basic manipulation
-
 transactions <- 
   read_sheet(ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
              sheet = "transactionLanding", 
-             col_types = "_cD____ddcc") %>% 
+             col_types = "_cD____ddcc_") %>% 
   replace_na( list("transactionDebit" = 0, "transactionCredit" = 0)) %>% 
   mutate("transactionBalance" = transactionCredit + transactionDebit) %>%
   mutate("trDate" = format(as.Date(transactionDate), "%Y-%m"))  %>%
   within(rm(transactionCredit, transactionDebit, transactionDate))
   
 # read budget sheet
-
 budget <- 
   read_sheet(ss = "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
              sheet = "budgetAnnual", 
@@ -43,26 +41,30 @@ monthlyTrackingDetail <- transactions %>%
   summarise(monthlyAmt = sum(transactionBalance))  
 
 # calculate the number of months for which there is data
-
 elapsedMonths <- as.numeric(length(unique(monthlyTracking$trDate)))
 
-# merge and manipulate monthly transactions with budget 
+# identify the last month of data
+lastMonth = max(transactions$trDate)
 
+# vector of colours used to fill bars based on % of monthly budget spent
+my_colours <- c("green", "blue", "orange") 
+
+# merge and manipulate monthly transactions with budget 
 monthlyCompare <- merge(x = monthlyTracking, y = budget, by = "transactionType") %>%
-  filter(transactionTypeCat == "Expense") %>%
+  filter(transactionTypeCat == "Expense") %>% 
+  filter(transactionType != "Misc") %>%
   within(rm(annualValueEst, transactionTypeCat)) %>%
   mutate("absDiff" = monthlyAmt + budgetMonthly) %>%
-  mutate("pctDiff" = 100 * (1 - ((monthlyAmt + budgetMonthly) / budgetMonthly))) 
+  mutate("pctDiff" = -100 * (1 - ((monthlyAmt + budgetMonthly) / budgetMonthly))) 
 
 
 # create breaks in a categorical variable to help with colouring the charts
-
 monthlyCompare$pctDiffCat <- cut(monthlyCompare$pctDiff,
                      breaks=c(-Inf, 75, 125, Inf),
                      labels=c("low","medium","high"))
 
-# pivot differences to compare by month
 
+# pivot differences to compare by month
 monthlyAbsDiff <- monthlyCompare %>%
   within(rm(monthlyAmt, pctDiff)) %>% 
   pivot_wider(names_from = trDate, values_from = "absDiff", values_fill = 0)
@@ -71,10 +73,7 @@ monthlyPctDiff <- monthlyCompare %>%
   within(rm(monthlyAmt, absDiff)) %>%
   pivot_wider(names_from = trDate, values_from = "pctDiff", values_fill = 0)
 
-# visualize monthly % diffences from budget in facet by month
-
-my_colours <- c("green", "blue", "orange") # vector of colours used to fill bars based on % of monthly budget spent
-
+# month vis % diffences from budget in facet by month
 bc <- ggplot(monthlyCompare) +
   geom_hline(yintercept = 100, colour = "grey", linetype = "longdash") +
   geom_bar(aes(x=transactionType, y=pctDiff, fill=pctDiffCat), stat='identity', show.legend = FALSE) +
@@ -87,29 +86,28 @@ bc <- ggplot(monthlyCompare) +
 bc 
     
 # ytd df similar to monthly
-
 ytdCompare <- merge(x = monthlyTracking, y = budget, by = "transactionType") %>%
+  filter(transactionType != "Misc") %>%
   group_by(transactionType) %>%
   summarise(ytdAmount = sum(monthlyAmt), meanBudget = mean(budgetMonthly)) %>%
   mutate("ytdBudget" = meanBudget * elapsedMonths) %>%
   mutate("ytdPctDiff" = -100 * (1 - ((ytdAmount + ytdBudget) / ytdBudget))) %>%
   arrange(abs(ytdBudget)) %>%    # First sort by val. This sort the dataframe but NOT the factor levels
-  mutate(transactionType = factor(transactionType, levels=transactionType)) %>%
+  mutate(transactionType = factor(transactionType, levels=transactionType))  %>%
   # create breaks in a categorical variable to help with colouring the charts
   mutate("pctDiffCat" = cut(ytdCompare$ytdPctDiff,
-                            breaks=c(-Inf, 75, 125, Inf),
-                            labels=c("low","medium","high"))) 
+                           breaks=c(-Inf, 75, 125, Inf),
+                           labels=c("low","medium","high"))) 
 
-lastMonth = max(transactions$trDate)
+
+
+## ytd vis
+
+# subtitle label identifying the number of months and last months
 stLT <- paste(elapsedMonths, "months up to", lastMonth, sep=" " )
+
+# label for bars in YTD chart
 ytdPDlabel <- percent(ytdCompare$ytdPctDiff / 100, accuracy = 1)
-
-write_sheet(monthlyCompare, ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
-            sheet = "monthlyCompare")
-write_sheet(ytdCompare, ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
-           sheet = "ytdCompare")
-
-# ytd vis
 
 ytdPCtVis <- ggplot(ytdCompare) +
   geom_hline(yintercept = 100, colour = "grey", linetype = "longdash") +
@@ -128,3 +126,9 @@ ytdPCtVis <- ggplot(ytdCompare) +
   coord_flip()
 ytdPCtVis
 
+
+# write dfs back to google sheets
+write_sheet(monthlyCompare, ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
+            sheet = "monthlyCompare")
+write_sheet(ytdCompare, ss= "1rsdSUAK7aN5_XxvlgcxuEpgyJNhbN8QJdAzJDxLhKbQ", 
+            sheet = "ytdCompare")
